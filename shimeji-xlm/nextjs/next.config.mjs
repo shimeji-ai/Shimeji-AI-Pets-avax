35c7e8e702921bc0b7ebb56a8275384a520ac33a
luloxi <lulox.eth@protonmail.com>
2026-02-22 19:02:59 +0000

    Fix auction route redirect

--- shimeji-xlm/nextjs/next.config.mjs
+++ shimeji-xlm/nextjs/next.config.mjs
@@ -6,20 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/auction",
-        destination: "/",
-        permanent: false,
-      },
-      {
-        source: "/auction/:path*",
-        destination: "/",
-        permanent: false,
-      },
-    ];
-  },
 }
 
 export default nextConfig
